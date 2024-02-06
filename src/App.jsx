17af8dfa8f415eb5ef1e570c68efe00344af50a1
Habeeb <skhabeebhussain91@.com>
2024-02-06 12:03:51 +0530

    Completed Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import "./App.css";
-import Carousel from "./components/Carousel";
-import React from "react";
+import { useState } from 'react'
+import './App.css'
+import Carousel from './components/Carousel'
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <Carousel />
-    </div>
-  );
+    <>
+       <div className="App">
+        <Carousel />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
